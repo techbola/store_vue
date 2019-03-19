10c8c602dec0d895ef1104b6ece3dbca5d75d16c
techbola <bfagbowore@gmail.com>
2019-03-19 15:21:39 +0100

    laravel backend with a vue frontend store, completed

---
+++ resources/js/app.js
@@ -0,0 +1,116 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import App from './components/App'
+import Home from './components/Home'
+import Login from './components/Login'
+import Register from './components/Register'
+import SingleProduct from './components/SingleProduct'
+import Checkout from './components/Checkout'
+import Confirmation from './components/Confirmation'
+import UserBoard from './components/UserBoard'
+import Admin from './components/Admin'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: Register
+        },
+        {
+            path: '/products/:id',
+            name: 'single-products',
+            component: SingleProduct
+        },
+        {
+            path: '/confirmation',
+            name: 'confirmation',
+            component: Confirmation
+        },
+        {
+            path: '/checkout',
+            name: 'checkout',
+            component: Checkout,
+            props: (route) => ({ pid: route.query.pid })
+        },
+        {
+            path: '/dashboard',
+            name: 'userboard',
+            component: UserBoard,
+            meta: {
+                requiresAuth: true,
+                is_user: true
+            }
+        },
+        {
+            path: '/admin/:page',
+            name: 'admin-pages',
+            component: Admin,
+            meta: {
+                requiresAuth: true,
+                is_admin: true
+            }
+        },
+        {
+            path: '/admin',
+            name: 'admin',
+            component: Admin,
+            meta: {
+                requiresAuth: true,
+                is_admin: true
+            }
+        },
+    ],
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (localStorage.getItem('bigstore.jwt') == null) {
+            next({
+                path: '/login',
+                params: { nextUrl: to.fullPath }
+            })
+        } else {
+            let user = JSON.parse(localStorage.getItem('bigstore.user'))
+            if (to.matched.some(record => record.meta.is_admin)) {
+                if (user.is_admin == 1) {
+                    next()
+                }
+                else {
+                    next({ name: 'userboard' })
+                }
+            }
+            else if (to.matched.some(record => record.meta.is_user)) {
+                if (user.is_admin == 0) {
+                    next()
+                }
+                else {
+                    next({ name: 'admin' })
+                }
+            }
+            next()
+        }
+    } else {
+        next()
+    }
+})
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router,
+});
---
+++ routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('/{any}', function(){
+        return view('landing');
+})->where('any', '.*');
